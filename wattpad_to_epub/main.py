57d3c42ba1e0be3ec22345873c79d374507fe38b
Marie <schindler.marie@gmail.com>
2021-11-15 21:37:22 +0100

    async with gather

--- wattpad_to_epub/main.py
+++ wattpad_to_epub/main.py
@@ -1,3 +1,4 @@
+import asyncio
 import re
 from pathlib import Path
 from typing import Any, Dict, Tuple
@@ -22,12 +23,12 @@ def get_id_from_url(url: str) -> Tuple[str, str]:
     return match.group("id", "slug")
 
 
-def make_chapter(client: httpx.Client, part: Dict[str, Any], index: int) -> epub.EpubHtml:
+async def create_chapter(client: httpx.Client, part: Dict[str, Any], index: int) -> epub.EpubHtml:
 
     chapter = epub.EpubHtml(title=part["title"], file_name=f"chapter_{index}.xhtml", lang="eng")
 
     logger.info(f"Downloading chapter {index}: {chapter.title} from {part['url']}...")
-    soup = BeautifulSoup(client.get(part["url"]))
+    soup = BeautifulSoup(await client.get(part["url"]), features="lxml")
 
     text = soup.pre
     text.name = "div"
@@ -45,7 +46,8 @@ def make_chapter(client: httpx.Client, part: Dict[str, Any], index: int) -> epub
             </head>
         <body></body>
         </html>
-    """
+        """,
+        features="lxml",
     )
     chapter_content.body.append(title_tag)
     chapter_content.body.append(text)
@@ -55,58 +57,59 @@ def make_chapter(client: httpx.Client, part: Dict[str, Any], index: int) -> epub
 
 @app.command()
 def get_story(url: str) -> None:
+    async def create_story(url: str):
+        story_id = get_id_from_url(url)
 
-    story_id = get_id_from_url(url)
+        # Download story infos with the API
+        async with httpx.AsyncClient() as client:
+            # change the user-agent header because wattpad blocks otherwise
+            client.headers["user-agent"] = "Mozilla/5.0"
 
-    # Download story infos with the API
-    client = httpx.Client()
-    # change the user-agent header because wattpad blocks otherwise
-    client.headers["user-agent"] = "Mozilla/5.0"
+            story_id, story_slug = get_id_from_url(url)
+            response = await client.get(f"{URL_STORY_API}/{story_id}")
+            story_info = response.json()
+            parts = story_info["parts"]
+            cover = await client.get(story_info["cover"])
+            cover = cover
 
-    story_id, story_slug = get_id_from_url(url)
-    response = client.get(f"{URL_STORY_API}/{story_id}")
-    story_info = response.json()
-    parts = story_info["parts"]
-    cover = client.get(story_info["cover"]).content
+            # Making the Epub file
+            epub_book = epub.EpubBook()
+            epub_book.set_identifier(f"fanfiction-{story_id}")
+            epub_book.set_title(story_info["title"])
+            epub_book.set_language(story_info["language"]["name"])
 
-    # Making the Epub file
-    epub_book = epub.EpubBook()
-    epub_book.set_identifier(f"fanfiction-{story_id}")
-    epub_book.set_title(story_info["title"])
-    epub_book.set_language(story_info["language"]["name"])
+            epub_book.add_author(story_info["user"]["name"])
+            epub_book.add_metadata("DC", "description", story_info["description"])
+            epub_book.add_metadata("DC", "publisher", "Wattpad")
+            epub_book.add_metadata("DC", "source", url)
 
-    epub_book.add_author(story_info["user"]["name"])
-    epub_book.add_metadata("DC", "description", story_info["description"])
-    epub_book.add_metadata("DC", "publisher", "Wattpad")
-    epub_book.add_metadata("DC", "source", url)
+            epub_book.set_cover("cover_image.jpg", cover.content)
 
-    epub_book.set_cover("cover_image.jpg", cover)
+            with open(STYLE_FILE_PATH) as css_file:
+                style_doc = epub.EpubItem(
+                    uid="style_doc", file_name="style/main.css", media_type="text/css", content=css_file.read()
+                )
+            epub_book.add_item(style_doc)
 
-    with open(STYLE_FILE_PATH) as css_file:
-        style_doc = epub.EpubItem(
-            uid="style_doc", file_name="style/main.css", media_type="text/css", content=css_file.read()
-        )
-    epub_book.add_item(style_doc)
+            chapters = await asyncio.gather(*[create_chapter(client, part, index) for index, part in enumerate(parts)])
 
-    chapters = []
+            for chapter in chapters:
+                chapter.add_item(style_doc)
+                epub_book.add_item(chapter)
 
-    for index, part in enumerate(parts):
-        chapter = make_chapter(client, part, index)
-        chapter.add_item(style_doc)
-        chapters.append(chapter)
-        epub_book.add_item(chapter)
+            epub_book.toc = (epub.Link("intro.xhtml", "Introduction", "intro"), (epub.Section("Chapters"), chapters))
+            epub_book.add_item(epub.EpubNcx())
+            epub_book.add_item(epub.EpubNav())
 
-    epub_book.toc = (epub.Link("intro.xhtml", "Introduction", "intro"), (epub.Section("Chapters"), chapters))
-    epub_book.add_item(epub.EpubNcx())
-    epub_book.add_item(epub.EpubNav())
+            nav_page = epub.EpubNav(uid="book_toc", file_name="toc.xhtml")
+            nav_page.add_item(style_doc)
+            epub_book.add_item(nav_page)
+            epub_book.spine = [nav_page] + chapters
 
-    nav_page = epub.EpubNav(uid="book_toc", file_name="toc.xhtml")
-    nav_page.add_item(style_doc)
-    epub_book.add_item(nav_page)
-    epub_book.spine = [nav_page] + chapters
+            # Write epub file
+            epub.write_epub(f"{story_slug}.epub", epub_book)
 
-    epub.write_epub(f"{story_slug}.epub", epub_book)
-    client.close()
+    asyncio.run(create_story(url))
 
 
 def main():
